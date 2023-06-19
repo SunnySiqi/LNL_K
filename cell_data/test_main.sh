a0b8bae6e0acc274e7c3d9cb94934e40115d5018
Siqi Wang <swang@rfc1918.address.not.used.bu.edu>
2023-06-19 18:50:24 -0400

    change path in test_main.sh

--- cell_data/test_main.sh
+++ cell_data/test_main.sh
@@ -14,7 +14,7 @@ TAG="crust_w_cdrp_lr-1"
 EVAL_DATASET='cdrp'
 NUM_PROCESSORS=12
 DATASET='cdrp'
-python cell_data/simple_multi_main.py --num_gpus 4 --num_processors ${NUM_PROCESSORS} --net 'full_effb0' \
+python simple_multi_main.py --num_gpus 4 --num_processors ${NUM_PROCESSORS} --net 'full_effb0' \
     --dataset ${DATASET} --eval_dataset ${EVAL_DATASET} --name ${TAG} --batch-size ${BATCH_SIZE}\
     --max_images_per_treatment 150 --dim_embed 672 --lr 1e-1 --epochs 50 --use_crust_k --fl-ratio 0.6\
     --start_epoch 10 --max_num_treatment 100 --predict-batch-size 32
