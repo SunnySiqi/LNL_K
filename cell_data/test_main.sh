b4ef94e4b8d076846fb7b5a016a964ce4caedc10
Siqi Wang <wangsiqi49@gmail.com>
2023-06-19 18:38:02 -0400

    change bash file

--- cell_data/test_main.sh
+++ cell_data/test_main.sh
@@ -12,9 +12,9 @@ env|grep -i cuda
 BATCH_SIZE=128
 TAG="crust_w_cdrp_lr-1"
 EVAL_DATASET='cdrp'
-NUM_PROCESSORS=8
+NUM_PROCESSORS=12
 DATASET='cdrp'
-python simple_multi_main.py --num_gpus 2 --num_processors ${NUM_PROCESSORS} --net 'full_effb0' \
+python cell_data/simple_multi_main.py --num_gpus 4 --num_processors ${NUM_PROCESSORS} --net 'full_effb0' \
     --dataset ${DATASET} --eval_dataset ${EVAL_DATASET} --name ${TAG} --batch-size ${BATCH_SIZE}\
     --max_images_per_treatment 150 --dim_embed 672 --lr 1e-1 --epochs 50 --use_crust_k --fl-ratio 0.6\
     --start_epoch 10 --max_num_treatment 100 --predict-batch-size 32
